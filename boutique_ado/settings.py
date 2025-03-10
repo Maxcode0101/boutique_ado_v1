2f8d7c82d5f3159abe57dc077b8105d419c44b70
MaxCode0101 <maximilian.kaening@gmail.com>
2025-03-10 11:00:47 +0100

    Tested webhook via stripe CLI

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = 'django-insecure-_#z4dv_l+()f5@qm_$b$^6id20@o-vceltp*8vdxv%&*918l%a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
@@ -178,6 +181,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
